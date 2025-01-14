b0f569857369d44f3148e0dbf8918690b0d2905c
surj358 <suraj.salve3011@gmail.com>
2025-01-14 14:43:14 +0530

    tailwind updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,13 @@ export default {
       },
       
     },
+    screens: {
+      sm:"0px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+      "2xl":"1536px",
+    }
   },
   
   plugins: [],
